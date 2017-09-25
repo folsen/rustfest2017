d0e5762ceddf29ea34829828ef225eedc8ee510c
Fredrik <ique@3h.se>
2017-09-25 14:44:17 +0200

    Add reference to docs

--- roguelike-tree-bot/src/main.rs
+++ roguelike-tree-bot/src/main.rs
@@ -11,6 +11,8 @@ static MOVE_STREAK: u32 = 5; // How many moves in a row it takes before adding n
 
 fn main() {
 	/*
+	See https://docs.rs/id_tree/1.1.3/id_tree/index.html for documentation on how to use the tree library
+
 	1. Create a decision-tree with `MAX_LEVELS` depth, and then start a game loop
 	2. Create a game with Game::new(false) to not print the board and Game::new(true) to print it
 	3. Use game.enter_move(move, true/false) to put in a move and optionally print it
